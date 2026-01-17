dc958cfa529635fca59ff24af521dd43fdcbd619
ItsIshan-1 <ishanzoci@gmail.com>
2026-01-17 16:57:35 +0530

    Add Vercel Analytics tracking

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -5,6 +5,7 @@ import { Toaster } from 'react-hot-toast';
 
 import { Routes, Route, useLocation } from "react-router-dom";
 import Footer from './components/Footer';
+import { Analytics } from '@vercel/analytics/react';
 import { useAppContext } from './context/AppContext';
 import Login from './components/Login';
 import AllProducts from './pages/AllProducts';
@@ -80,6 +81,7 @@ const App = () => {
 
       </div>
       {!isSellerPath && <Footer />}
+      <Analytics />
     </div>
 
   )
